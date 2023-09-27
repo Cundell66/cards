1bb1855cc6576feaf69c7dc8041362fe93d9b1d9
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-09-27 16:35:50 +0100

    write and read file complete

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
+// import "fmt"
+
 func main() {
 
-	
-	cards := newDeck()
+	// cards := newDeck()
+	// cards.saveToFile("cardDeck")
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
 
+	cards := newDeckFromFile("cardDeck")
+	cards.print()
 }
